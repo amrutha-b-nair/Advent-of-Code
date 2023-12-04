5fd05201c2f3a43a998a5de21fa6f7f3ae9e5347
amrutha-b-nair <amruthabn00@gmail.com>
2023-12-04 08:06:19 +0100

    Day 4

--- 2023/day4/solution.py
+++ 2023/day4/solution.py
@@ -4,16 +4,6 @@ with open('input.txt') as file:
     lines = file.read().strip().split('\n')
 
 total_score = 0
-for line in lines:
-    card = re.split(r'[:|]', line)
-    card_winning = card[1].strip().split(' ')
-    card_elf = card[2].strip().split(' ')
-    winning_numbers = sum(1 for number in card_elf if number in card_winning and number != '')
-    if winning_numbers != 0:
-        total_score += 2**(winning_numbers- 1)
-
-print("Part 1: ",total_score)
-
 card_pile = {key : 1 for key in range(1, len(lines) + 1)}
 for line in lines:
     card = re.split(r'[:|]', line)
@@ -21,8 +11,12 @@ for line in lines:
     card_winning = card[1].strip().split(' ')
     card_elf = card[2].strip().split(' ')
     winning_numbers = sum(1 for number in card_elf if number in card_winning and number != '')
+    if winning_numbers != 0:
+        total_score += 2**(winning_numbers- 1)
     for i in range(winning_numbers):
         card_pile[card_id + i + 1] += 1*card_pile[card_id]
+
+print("Part 1: ",total_score)
 
 card_total = sum(value for value in card_pile.values())
 print("Part 2: ",card_total)
