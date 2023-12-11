3e03489b5a58c319c50c3ae2771d8b35f904a487
amrutha-b-nair <amruthabn00@gmail.com>
2023-12-11 10:30:38 +0100

    Day 11

--- 2023/day11/solution.py
+++ 2023/day11/solution.py
@@ -11,12 +11,12 @@ def empty_indices(input_array):
             cols.append(j)
     return rows, cols
 
-def shortest_dist(input_array):
+def shortest_dist(input_data):
     empty_rows, empty_cols = empty_indices(input_data)
     row_indices, col_indices = np.where(input_data == '#')
     total_length = 0
     count = 0
-    for i in range(len(row_indices)):
+    for i in range(len(row_indices) - 1):
         for j in range(i+1, len(row_indices)):
             for index in empty_rows:
                 if row_indices[i] < index < row_indices[j] or row_indices[j] < index < row_indices[i]:
@@ -25,6 +25,7 @@ def shortest_dist(input_array):
                 if col_indices[i] < index < col_indices[j] or col_indices[j] < index < col_indices[i]:
                     count += 1
             total_length += abs(row_indices[i]-row_indices[j]) + abs(col_indices[i]-col_indices[j])
+    
     return total_length, count
 
 with open('input.txt') as file:
