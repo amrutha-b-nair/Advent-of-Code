f765ece873f9600432e1cc428d724e297abc81eb
amrutha-b-nair <amruthabn00@gmail.com>
2023-12-15 16:27:42 +0100

    Day 13

--- 2023/day13/solution.py
+++ 2023/day13/solution.py
@@ -1,97 +1,97 @@
 from itertools import takewhile
 import numpy as np
 
-def is_reflection(count_list):
 
-    leading_zeros = len(list(takewhile(lambda x: x == 0, count_list)))
-    trailing_zeros = len(list(takewhile(lambda x: x == 0, reversed(count_list))))
-    if leading_zeros != 0:
-        return leading_zeros
-    elif trailing_zeros != 0:
-        return (len(count_list) - trailing_zeros)
+def is_palindrome(lines, index):
+    palindrom_dict ={}
+    for i in range(index):
+        if lines[i] not in palindrom_dict:
+            palindrom_dict[lines[i]] = 1
+        else:
+            del palindrom_dict[lines[i]]
+    if len(palindrom_dict) == 0:
+        return True
     else:
-        return 0
+        return False
 
 def row_reflection(lines):
-    visited = []
-    indices = [1 for _ in range(len(lines))]
-    palindrome = False
-    palindrome_start = None
-    for i,line in enumerate(lines):
-        if line not in visited:
-            # print('$$$$', i)
-            visited.append(line)
-            if palindrome:
-                # print('palindrome')
-                if lines[i-1] == lines[0]:
-                    # print('!!!!!!!!!!',i)
-                    return i//2
-                
-                for k in range(i):
-                    indices[k] = 1
-                palindrome = False
-        else:
-            if palindrome:
-                if indices[i-1] != 0:
-                    # print('#####',i)
-                    indices[i] = 1
-                    palindrome = False
-                else:
-                    if lines[i] == lines[2*palindrome_start - i- 1]:
-                        # print('^^^^^',i)
-                        indices[i] = 0
+    possible_indices = [i for i in range(1, len(lines)) if lines[0] == lines[i]]
+    if len(possible_indices) == 0:
+        return None
+    else:
+        for index in possible_indices:
+            if is_palindrome(lines, index + 1):
+                return (index +1)//2
+    return None
 
-            elif palindrome == False:
-                if visited[-1] == line:
-                    palindrome_start = i
-                    # print('&&&&', i)
-                    palindrome = True
-                    indices[i] = 0
-                else:
-                    # print('((()))', i)
-                    visited.append(line)
-
-    # print(indices)
-    return is_reflection(indices)
-
-def col_reflection(lines):
-    arr = np.array([list(row) for row in lines])
-    transposed = [''.join(row) for row in arr.T]
-    return row_reflection(transposed)
 
 def get_summary(input_data):
+    reflection_lines = []
     summerize = 0
     for input in input_data:
-        # print(input, '\n\n')
         lines = input.split('\n')
-        nrows = row_reflection(lines)
-        if nrows == 0:
-            ncols = col_reflection(lines)
-            summerize += ncols
-        else:
-            # print(nrows)
-            summerize += 100*(nrows)
+        for i in range(4):
+            arr = np.array([list(row) for row in lines])
+            rotated = [''.join(row) for row in np.rot90(arr, k = i)]
+            index = row_reflection(rotated)
+
+            if index != None:
+                reflection_lines.append([i,index])
+                if i == 0:
+                    summerize += index*100 
+                elif i == 1:
+                    summerize += len(rotated) - index
+                elif i == 2:
+                    summerize += (len(rotated) - index)*100
+                elif i == 3:
+                    summerize += index
+                break
+
+    return summerize, reflection_lines
+
+            
+
+def smudged_line(lines):
+    for i in range(len(lines)-1):
+        for j in range(i + 1, len(lines)):
+            diff_count = sum(x != y for x, y in zip(lines[i], lines[j]))
+            if diff_count == 1:
+                index = next(k for k, (x, y) in enumerate(zip(lines[i], lines[j])) if x != y)
+                char_map = {'#': '.', '.': '#'}
+                new_lines = lines.copy()
+                new_lines[i] = lines[i][:index] + char_map.get(lines[i][index], lines[i][index]) + lines[i][index + 1:]
+                index = row_reflection(new_lines)
+                if index != None:
+                    return index
+    return None
+
+
+def get_smudged_summary(input_data, reflection_lines):
+    summerize = 0
+    for j,input in enumerate(input_data):
+        lines = input.split('\n')
+        for i in range(4):
+            arr = np.array([list(row) for row in lines])
+            rotated = [''.join(row) for row in np.rot90(arr, k = i)]
+            index = smudged_line(rotated)
+            if index != None and reflection_lines[j] != [i,index]:
+                if i == 0:
+                    summerize += index*100 
+                elif i == 1:
+                    summerize += len(rotated) - index
+                elif i == 2:
+                    summerize += (len(rotated) - index)*100
+                elif i == 3:
+                    summerize += index
+                break
     return summerize
 
 
-# def get_smudged_summary(input_data):
-#     for input in input_data:
-#         lines = input.split('\n')
-#         for i in range(len(lines)):
-#             for j in range(i, len(lines)):
-#                 difference = 0
-#                 for k in range(len(lines[i])):
-#                     if lines[i][k] != lines[j][k]:
-#                         difference += 1
-#                         if difference > 1:
-#                             break
 
-
-
-
-with open('trial.txt') as file:
+with open('input.txt') as file:
     input_data = file.read().strip().split('\n\n')
 
 
-print(get_summary(input_data))
-print(get_smudged_summary(input_data))
+summary, reflection_lines = get_summary(input_data)
+print('Part 1:',summary)
+print('Part 2:', get_smudged_summary(input_data, reflection_lines))
--- 2023/day13/testing.py
+++ 2023/day13/testing.py
@@ -4,7 +4,7 @@ from solution import get_summary
 class YourTests(unittest.TestCase):
 
     def test_case_1(self):
-        input_data = '##..#.#\n###.#.#\n###.#.#\n##..#.#\n#.##.#.\n.#....#\n..#.###\n..#.#.#\n.##..#.\n....###\n###....\n#..#.\##\n######.\n.##..##\n#.###.#'.strip().split('\n\n')
+        input_data = '##..#.#\n###.#.#\n###.#.#\n##..#.#\n#.##.#.\n.#....#\n..#.###\n..#.#.#\n.##..#.\n....###\n###....\n#..#.##\n######.\n.##..##\n#.###.#'.strip().split('\n\n')
         expected_output = 200
         result = get_summary(input_data)
         self.assertEqual(result, expected_output)
@@ -22,17 +22,5 @@ class YourTests(unittest.TestCase):
         self.assertEqual(result, expected_output)
 
     
-    # def test_case_3(self):
-    #     input_data = '...........\n.S-------7.\n.|F-----7|.\n.||.....||.\n.||.....||.\n.|L-7.F-J|.\n.|..|.|..|.\n.L--J.L--J.\n...........'
-    #     expected_output = 4
-    #     result = part_two(input_data)
-
-    #     self.assertEqual(result, expected_output)
-
-    # def test_case_4(self):
-    #     input_data = '.F----7F7F7F7F-7....\n.|F--7||||||||FJ....\n.||.FJ||||||||L7....\nFJL7L7LJLJ||LJ.L-7..\nL--J.L7...LJS7F-7L7.\n....F-J..F7FJ|L7L7L7\n....L7.F7||L7|.L7L7|\n.....|FJLJ|FJ|F7|.LJ\n....FJL-7.||.||||...\n....L---J.LJ.LJLJ...'
-    #     expected_output = 8
-    #     result = part_two(input_data)
-    #     self.assertEqual(result, expected_output)
 if __name__ == '__main__':
     unittest.main()
