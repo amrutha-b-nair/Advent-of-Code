a2a8ce1e516426949db3c4c64f08c663d331baff
amrutha-b-nair <amruthabn00@gmail.com>
2024-12-04 08:26:13 +0100

    part 2

--- 2024/day2/solution.py
+++ 2024/day2/solution.py
@@ -1,50 +1,24 @@
+from collections import Counter
+
 with open('input.txt') as file:
     lines = file.read().strip().split('\n')
 
-################### Part 1 #####################
+
+def isReportSafe(report):
+    diff = [report[i + 1] - report[i] for i in range(len(report) - 1)]
+    if set(diff) <= {1, 2, 3} or set(diff) <= {-1, -2, -3}:
+        return True
+    return False
+
+reports = [[int(data) for data in line.split(" ")] for line in lines]
 
 
-# notSafe = 0
+safeReportCount = sum([isReportSafe(report) for report in reports])
+print("Part 1:", safeReportCount)
 
-# for line in lines:
-#     report = [int(data) for data in line.split(" ")]
-#     print([report[i+1] - report[i] for i in range(len(report)-1)])
+safeReportCount = sum([any([isReportSafe(report[:i] + report[i + 1:]) for i in range(len(report))]) for report in reports])
+print("Part 2:", safeReportCount)
 
-#     ## increasing : sign = 1, else -1
-#     if 1<= abs(report[1] - report[0]) <= 3:
-#         sign = (report[1] - report[0]) / abs(report[0]-report[1])
-        
-#         for x, y in zip(report, report[1:]):
-#             if (y - x) in [sign*1, sign*2, sign*3]:
-#                 continue
-#             else:
-#                 notSafe += 1
-#                 break
-#     else:
-#         notSafe += 1
-
-
-# print(len(lines) - notSafe)
-
-################### Part 2 #####################
-safe = 0
-
-getSign = lambda x: 1 if x > 0 else -1 if x < 0 else 0
-
-great = 0
-
-for line in lines:
-    report = [int(data) for data in line.split(" ")]
-    
-    diff = {report[i+1] - report[i]: getSign(report[i+1] - report[i]) for i in range(len(report)-1)}
-    if set(diff.keys()) <= {diff[next(iter(diff))]*1, diff[next(iter(diff))]*2, diff[next(iter(diff))]*3}:
-        great += 1
-        continue
-    
-
-
-
-print(safe, great)
 
 
 
