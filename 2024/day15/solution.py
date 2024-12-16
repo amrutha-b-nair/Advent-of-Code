b6c9a8d281a52f3a7153c7a67ced0384156de626
amrutha-b-nair <amruthabn00@gmail.com>
2024-12-16 14:52:01 +0100

    part2

--- 2024/day15/solution.py
+++ 2024/day15/solution.py
@@ -1,3 +1,4 @@
+
 import copy
 with open('input.txt') as file:
     map, moves  = file.read().strip().split("\n\n")
@@ -37,8 +38,83 @@ def canMove(grid, boxPos, dir):
         else:
             boxPos = nextPos
 
+def moveHorizontal(grid, pos, dir):
+    copyGrid = copy.deepcopy(grid)
 
-def partOne(map, moves, robot):
+    while(True):
+        nextPos = getNextPos(pos, dir)
+        if grid[nextPos] == ".":
+            copyGrid[nextPos] = grid[pos]
+            return copyGrid
+        elif grid[nextPos] == "#":
+            return False
+        else:
+            copyGrid[nextPos] = grid[pos]
+            pos = nextPos
+
+
+def moveVertical(grid, pos, move):
+    copyGrid = copy.deepcopy(grid)
+    if grid[pos] == "[":
+        posOther = (pos[0], pos[1] + 1)
+    elif grid[pos] == "]":
+        posOther = (pos[0], pos[1] - 1)
+
+    boxes = {pos, posOther}
+    visited = [pos, posOther]
+    changed = set()
+    while(len(boxes) > 0):
+        curr = boxes.pop()
+        nextPos = getNextPos(curr, move)
+        visited.append(curr)
+        if grid[curr] == "[":
+            posTwo = (curr[0], curr[1] + 1)
+            if posTwo not in visited:
+                boxes.add(posTwo)
+        elif grid[curr] == "]":
+            posTwo = (curr[0], curr[1] - 1)
+            if posTwo not in visited:
+                boxes.add(posTwo)
+        if grid[nextPos] == ".":
+            copyGrid[nextPos] = grid[curr]
+            changed.add(nextPos)
+            continue
+        elif grid[nextPos] == "#":
+            return False
+        else:
+            if nextPos not in visited:
+                boxes.add(nextPos)
+            copyGrid[nextPos] = grid[curr]
+            changed.add(nextPos)
+            curr = nextPos
+    for x in visited:
+        if x not in changed:
+            copyGrid[x] = "."
+            changed.add(x)
+    copyGrid[pos] = "@"
+    copyGrid[posOther] = "."
+    return copyGrid
+    
+
+
+def moveBox(grid, robo, move):
+    if move == "<" or move == ">":
+        moved = moveHorizontal(grid, robo, move)
+        if moved != False:
+            moved[robo] = "."
+            return moved
+    else:
+        nextPos = getNextPos(robo, move)
+        moved = moveVertical(grid, nextPos, move)
+        if moved != False:
+            moved[robo] = "."
+            return moved
+    return False
+
+        
+
+def partOne(grid, moves, robot):
+    map = copy.deepcopy(grid)
     for move in moves:
         if move not in directions.keys():
             continue
@@ -48,7 +124,7 @@ def partOne(map, moves, robot):
             map[nextPos] = "@"
             robot = nextPos
         elif map[nextPos] == "O":
-            pos = canMove(grid, nextPos, move)
+            pos = canMove(map, nextPos, move)
             if pos != False:
                 map[pos] = "O"
                 map[robot] = "."
@@ -64,5 +140,53 @@ def partOne(map, moves, robot):
     print("Part 1:",total)
 
 
+
+def getNewGrid(grid):
+    newGrid = {}
+    for i in range(nrows):
+        for j in range(ncols):
+            if grid[(i,j)] == "#":
+                newGrid[(i, 2*j)] = "#"
+                newGrid[(i, 2*j+1)] = "#"
+            elif grid[(i,j)] == "O":
+                newGrid[(i, 2*j)] = "["
+                newGrid[(i, 2*j+1)] = "]"
+            elif grid[(i,j)] == ".":
+                newGrid[(i, 2*j)] = "."
+                newGrid[(i, 2*j+1)] = "."
+            else:
+                newRobot = (i, 2*j)
+                newGrid[(i, 2*j)] = "@"
+                newGrid[(i, 2*j+1)] = "."
+    return newGrid, newRobot
+
+
+def partTwo(map, moves):
+    newGrid, newRobot = getNewGrid(map)
+
+    for move in moves:
+        if move not in directions.keys():
+            continue
+        nextPos = getNextPos(newRobot, move)
+        if newGrid[nextPos] == ".":
+            newGrid[newRobot] = "."
+            newGrid[nextPos] = "@"
+            newRobot = nextPos
+        elif newGrid[nextPos] == "[" or newGrid[nextPos] == "]":
+            movedGrid = moveBox(newGrid, newRobot, move)
+            if movedGrid != False:
+                newGrid = movedGrid
+                newRobot = nextPos
+                
+    
+    total = 0
+
+    for pos, value in newGrid.items():
+        if value == "[":
+            total += (pos[1] + 100*pos[0])
+
+    print("Part 2:",total)
+
+
 partOne(grid, moves, robot)
-# partTwo(grid, moves)
+partTwo(grid, moves)
